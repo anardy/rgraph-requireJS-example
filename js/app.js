792ceb57f3b18d19c1891a95db5473cf3110d0a7
anardy <macknardy@gmail.com>
2014-10-30 19:20:10 -0200

    remove archives

--- js/app.js
+++
@@ -1,7 +0,0 @@
-require(['gauge', 'rgraph'], function(Gauge, RGraph) {
-
-    var CreateGauge = function() {
-    	var graph = new RGraph.Gauge('graph', 0, 100, 10);
-    	graph.Draw();
-    }();
-});
